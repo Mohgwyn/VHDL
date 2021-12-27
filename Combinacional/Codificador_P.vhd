19dd373f79bcc159b9b6a1955ed86ae552ef8ced
jsonofbitch <94811693+jsonofbitch@users.noreply.github.com>
2021-12-27 01:27:22 +0100

    Update Codificador_P.vhd

--- Combinacional/Codificador_P.vhd
+++ Combinacional/Codificador_P.vhd
@@ -1,36 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 29.11.2021 19:42:28
--- Design Name: 
--- Module Name: Codificador_P - B
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Codificador_P is
     generic(n: integer := 2);
