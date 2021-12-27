c1aae1cd40f786cbb70f8aba484e8e3e2881b69e
jsonofbitch <94811693+jsonofbitch@users.noreply.github.com>
2021-12-27 01:28:49 +0100

    Update ROM.vhd

--- Combinacional/ROM.vhd
+++ Combinacional/ROM.vhd
@@ -1,37 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.12.2021 00:27:14
--- Design Name: 
--- Module Name: ROM - Comportamiento
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.Tipos_ROM_MUX.all;
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity ROM is
     generic( N_Bits_Dir : natural := 2);
