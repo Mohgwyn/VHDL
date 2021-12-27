4b25aa4ec465c4c3f9bbca68fd4097b647967a2e
jsonofbitch <94811693+jsonofbitch@users.noreply.github.com>
2021-12-27 01:28:15 +0100

    Update MUX.vhd

--- Combinacional/MUX.vhd
+++ Combinacional/MUX.vhd
@@ -1,36 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.12.2021 00:34:35
--- Design Name: 
--- Module Name: MUX - Comportamiento
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use work.Tipos_ROM_MUX.all;
 use IEEE.NUMERIC_STD.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity MUX is
     generic( N_Bits_Dir : Natural := 2);
